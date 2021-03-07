7a1291d03d21bcc9d5a913be421a1b15c04e7667
koengreen <302261150@student.rocmondriaan.nl>
2021-03-07 01:27:29 +0100

    Higher lower game

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Would you like to play the Higher/Lower game? (Yes/No)");
+        string userInput = Console.ReadLine();
+
+        if (userInput == "No")
+        {
+            Console.WriteLine("Goodbye!");
+        }
+        else if (userInput == "Yes")
+        {
+            Console.WriteLine("Pick a number from 1 to 100");
+            int start = 1;
+            int end = 100;
+            Random rnd = new Random();
+            int number = rnd.Next(start, end);
+
+            while (true)
+            {
+                Console.WriteLine("Is your number higher or lower than " + number + "? (Higher/Lower/Correct)");
+                string answerInput = Console.ReadLine();
+                if (answerInput == "Higher")
+                {
+                    start = number + 1;
+                    number = rnd.Next(start, end);
+                }
+                else if (answerInput == "Lower")
+                {
+                    end = number - 1;
+                    number = rnd.Next(start, end);
+                }
+                else if (answerInput == "Correct")
+                {
+                    Console.WriteLine("Great! I guessed your number.");
+                    break;
+                }
+            }
+            Main();
+        }
+        else
+        {
+            Console.Error.WriteLine("Error. Wrong input. Try again.");
+        }
+    }
+}
